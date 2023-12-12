0c8706ec5ec840f636152ada5de3c03b7a61f37e
HoruKujikawa <152450649+HoruKujikawa@users.noreply.github.com>
2023-12-12 18:48:52 -0500

    Update visualizar.php

--- visualizar.php
+++ visualizar.php
@@ -68,6 +68,7 @@
           </table>
           <div class=""container>
             <a href="Agregarformulario.php" class="btn btn-success">Agregar Producto</a>
+            <a href="MenuV.html" class="btn btn-primary">Regresar</a>
                 </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
