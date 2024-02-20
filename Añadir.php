3c2f100349d99f9918fb1e328115a1e215e27b04
HoruKujikawa <152450649+HoruKujikawa@users.noreply.github.com>
2024-02-20 00:24:04 -0500

    Update Añadir.php

--- Añadir.php
+++ Añadir.php
@@ -7,17 +7,24 @@ $color = $_POST["color"];
 $cantidad = $_POST["cantidad"];
 $descripcion = $_POST["descripcion"];
 $nombre = $_POST["nombre"];
-$marca = $_POST["marca"];
+$nombre_marca = $_POST["nombre_marca"];
 $estado = $_POST["estado"];
 $categorias = $_POST["categorias"];
 
-$insertar = "INSERT INTO producto(talla,color,cantidad,descripcion,nombre,marca,estado,categorias)VALUES('$talla', '$color', '$cantidad', '$descripcion', '$nombre', '$marca', '$estado', '$categorias')";
+$insertar = "INSERT INTO producto(talla,color,cantidad,descripcion,nombre,estado,categorias)VALUES('$talla', '$color', '$cantidad', '$descripcion', '$nombre', '$estado', '$categorias')";
 $resultado = mysqli_query($conectar, $insertar);
 
-if ($resultado === TRUE){
-    header("location: visualizar.php");
-}else{
- echo "Datos NO insertados";
-}
+$consultaid = "select id_producto from producto where talla = '$talla' and color = '$color' and cantidad = '$cantidad' and descripcion = '$descripcion' and nombre = '$nombre' and estado = '$estado' and categorias = '$categorias'";
+$resultadoid = mysqli_query($conectar, $consultaid);
 
+    // Obtener el valor directamente
+    $row = $resultadoid->fetch_assoc();
+    $valor = $row["id_producto"];
+
+    echo "Valor encontrado: " . $valor;
+    echo "Valor marca: " . $nombre_marca;
+$insertar2 = "INSERT INTO marca_producto(fk_id_producto, fk_id_marca)VALUES($valor,$nombre_marca)";
+$resultado2 = mysqli_query($conectar, $insertar2);
+
+header("Location: visualizar.php");
 ?>
